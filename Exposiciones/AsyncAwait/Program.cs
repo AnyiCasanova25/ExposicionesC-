048c56d280c2606bd2c3e14afe61babf4775d284
Anyi Zujey <anyi22195@gmail.com>
2024-08-16 13:15:36 -0500

    3,4,5

--- Exposiciones/AsyncAwait/Program.cs
+++ Exposiciones/AsyncAwait/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Program
+{
+    static async Task Main(string[] args)
+    {
+        Console.WriteLine("Iniciando descarga");
+        Console.WriteLine("Esto podria tardar 5 segundos");
+
+        var resultado = await BaseFalsa();
+        Console.WriteLine(resultado);
+    }
+
+    static async Task<string> BaseFalsa()
+    {
+        await Task.Delay(5000);
+        return "Descarga finalizada con exito!!!";
+    }
+}
---
+++ Exposiciones/SRP(1)/Temperatura.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SRP_1_
+{
+    internal class Temperatura
+    {
+    }
+}
